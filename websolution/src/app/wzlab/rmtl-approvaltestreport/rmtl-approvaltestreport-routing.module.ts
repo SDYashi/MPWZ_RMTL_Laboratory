82232b91c1bab43807f5fc33a63cdbe52d88c60c
SDYashi <deepakmarskole88@gmail.com>
2025-10-15 14:05:21 +0530

    all model updated

--- websolution/src/app/wzlab/rmtl-approvaltestreport/rmtl-approvaltestreport-routing.module.ts
+++ websolution/src/app/wzlab/rmtl-approvaltestreport/rmtl-approvaltestreport-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RmtlApprovaltestreportDashboardComponent } from './rmtl-approvaltestreport-dashboard/rmtl-approvaltestreport-dashboard.component';
 const routes: Routes = [
   {path:'',redirectTo:'view-pending-test-reports',pathMatch:'full'},
   {path:'view-pending-test-reports',loadChildren:()=>import('./rmtl-testreport-pending/rmtl-testreport-pending-routing.module').then(m=>m.RmtlTestreportPendingRoutingModule)},
   {path:'view-approved-test-reports',loadChildren:()=>import('./rmtl-testreport-approved/rmtl-testreport-approved.module').then(m=>m.RmtlTestreportApprovedModule)},
-  {path:'view-oic-dashboard',component:RmtlApprovaltestreportDashboardComponent},
+
 
 ];
 
--- websolution/src/app/wzlab/rmtl-approvaltestreport/rmtl-approvaltestreport.module.ts
+++ websolution/src/app/wzlab/rmtl-approvaltestreport/rmtl-approvaltestreport.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RmtlApprovaltestreportRoutingModule } from './rmtl-approvaltestreport-routing.module';
 import { FormsModule } from '@angular/forms';
-import { RmtlApprovaltestreportDashboardComponent } from './rmtl-approvaltestreport-dashboard/rmtl-approvaltestreport-dashboard.component';
 
 
 @NgModule({
   declarations: [
-  
-    RmtlApprovaltestreportDashboardComponent
   ],
   imports: [
     CommonModule,
